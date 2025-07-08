ab4861242599423677b6804f8747b28abdfd14ca
waelbilal2024 <166135368+waelbilal2024@users.noreply.github.com>
2025-07-08 10:56:42 +0300

    Initial commit

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+PORT=${PORT:-8080}
+
+# تأخير لضمان جاهزية الشبكة (للمشاكل المتكررة)
+sleep 10
+
+echo "🚀 Starting Rasa on port $PORT"
 rasa run --enable-api --cors "*" --port $PORT --debug
