ab7fb1426d4aa6185601608283b687d304fb0781
waelbilal2024 <166135368+waelbilal2024@users.noreply.github.com>
2025-07-13 17:31:11 +0300

    Initial commit

--- start.sh
+++ start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
 # تشغيل خادم Rasa في الخلفية
-rasa run --enable-api --cors "*" --port $PORT &
-
-# تشغيل خادم Actions
-rasa run actions --port $ACTION_PORT
-
-# يمكنك إضافة تحقق من الأخطاء هنا إذا لزم الأمر
+rasa run --enable-api --cors "*" --port $PORT 
